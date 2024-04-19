b2a567c8dfadde8875c4d9ab76acb238b9aa285e
tsurma <tsurma@student.42berlin.de>
2024-04-19 16:42:44 +0200

    further opitmisations and fixes

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,12 +6,13 @@
 /*   By: tsurma <tsurma@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 10:09:34 by elisevanite       #+#    #+#             */
-/*   Updated: 2024/04/19 15:46:43 by tsurma           ###   ########.fr       */
+/*   Updated: 2024/04/19 16:37:45 by tsurma           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/builtins.h"
 
+static int	go_home(t_meta *meta);
 static int	get_home(t_meta *meta);
 
 int	ft_cd(char **args, t_meta *meta)
@@ -34,14 +35,23 @@ int	ft_cd(char **args, t_meta *meta)
 		return (0);
 	}
 	else
+		return (go_home(meta));
+}
+
+static int	go_home(t_meta *meta)
+{
+	int	i;
+
+	i = get_home(meta);
+	if (i < 0)
+		return (-1);
+	if (chdir(meta->envp[i] + 5) != 0)
 	{
-		if (chdir(meta->envp[get_home(meta)] + 5) != 0)
-		{
-			ft_putstr_fd("Error cd\n", STDERR_FILENO);
-			return (-1);
-		}
-		return (0);
+		ft_putstr_fd("Error cd\n", STDERR_FILENO);
+		return (-1);
 	}
+	return (0);
+
 }
 
 static int	get_home(t_meta *meta)
@@ -55,6 +65,14 @@ static int	get_home(t_meta *meta)
 			return (i);
 		i++;
 	}
-	exit_error("HOME isn't set", NULL, 1, meta);
+	ft_putstr_fd("minishell: cd: Home not set\n", STDERR_FILENO);
 	return (-1);
+	{
+		if (chdir(meta->envp[get_home(meta)] + 5) != 0)
+		{
+			ft_putstr_fd("Error cd\n", STDERR_FILENO);
+			return (-1);
+		}
+		return (0);
+	}
 }
