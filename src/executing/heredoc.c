a6bc3187f8767af68a950b052fce417506c5a37b
elise <e.vaniterson98@gmail.com>
2024-05-10 11:16:30 +0200

    fix norminette heredoc.c

--- src/executing/heredoc.c
+++ src/executing/heredoc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   heredoc.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tsurma <tsurma@student.42.fr>              +#+  +:+       +#+        */
+/*   By: evan-ite <evan-ite@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 12:38:41 by evan-ite          #+#    #+#             */
-/*   Updated: 2024/05/06 13:55:01 by tsurma           ###   ########.fr       */
+/*   Updated: 2024/05/10 11:15:27 by evan-ite         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,36 @@ void	sigint_handler_heredoc(int sig)
 	}
 }
 
+/* Function to catch the heredoc input until the delimiter
+is entered or a signal is catched. */
+static void	loop_heredoc(int i, t_node *node)
+{
+	char	*line;
+
+	while (1 && !g_sig)
+	{
+		ft_putstr_fd("heredoc> ", STDOUT_FILENO);
+		line = get_next_line(STDIN_FILENO);
+		if (line == NULL)
+		{
+			ft_putstr_fd("warning: here-document delimited by end-of-"
+				"file\n", STDERR_FILENO);
+			break ;
+		}
+		if (!ft_strncmp(line, node->heredoc[i], ft_strlen(node->heredoc[i])))
+		{
+			free(line);
+			break ;
+		}
+		ft_putstr_fd(line, node->hd_pipe[i][1]);
+		free(line);
+	}
+}
+
 /* Returns 1 if the node contains a heredoc and arranges all input and output.
 If there's no heredoc the function returns 0. */
 int	check_heredoc(t_node *node, t_meta *meta)
 {
-	char	*line;
 	int		i;
 
 	if (!node->heredoc)
@@ -39,24 +64,7 @@ int	check_heredoc(t_node *node, t_meta *meta)
 			node->hd_pipe[i] = gnl_calloc(2, sizeof(int));
 			if (pipe(node->hd_pipe[i]) == -1)
 				exit_error(ERR_PIPE, NULL, 1, meta);
-			while (1 && !g_sig)
-			{
-				ft_putstr_fd("heredoc> ", STDOUT_FILENO);
-				line = get_next_line(STDIN_FILENO);
-				if (line == NULL)
-				{
-					ft_putstr_fd("warning: here-document delimited by end-of-"
-						"file\n", STDERR_FILENO);
-					break ;
-				}
-				if (!ft_strncmp(line, node->heredoc[i], ft_strlen(node->heredoc[i])))
-				{
-					free(line);
-					break ;
-				}
-				ft_putstr_fd(line, node->hd_pipe[i][1]);
-				free(line);
-			}
+			loop_heredoc(i, node);
 			ft_close(node->hd_pipe[i][1]);
 		}
 		i++;
